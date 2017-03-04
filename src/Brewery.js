9b2abf92c19ceb7bd38358979b2b75267c7837a8
cwalkersp <cwalker@singlplatform.com>
2017-03-04 13:44:27 -0500

    WIP Added URL parsing for values w/ missing protocols

--- src/Brewery.js
+++ src/Brewery.js
@@ -6,14 +6,19 @@ class Brewery extends Component {
     return (
       <div className="brewery">
         <span className={'circle ' + brewery.red_yellow_green.toLowerCase()}></span>
-        {/* TODO - Deal with url parsing the value in href */}
-        <span><a target="_blank" href={brewery.url}>{brewery.company_name}</a></span>
+        <span><a target="_blank" href={this.breweryUrl()}>{brewery.company_name}</a></span>
         -
         <span>{brewery.status}</span>
         {/* TODO - Deal with this JSON data - maybe a modal?? */}
         <span><a target="_blank" className='more-info' href={this.infoLink()}>More Info</a></span>
       </div>
     );
+  }
+
+  breweryUrl() {
+    let url = this.props.brewer.url
+    // Prepend protocol agnostic double slash if no protocol present
+    return (url.startsWith('http') ? '' : '//') + url;
   }
 
   infoLink() {
