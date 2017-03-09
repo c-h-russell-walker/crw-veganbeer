235de72c3f9929096a7714115e8395dfda7c0fe0
Chris Russell-Walker <c.russell.walker@gmail.com>
2017-03-08 20:47:49 -0500

    Format address with correct amount of commas

--- src/BrewerContact.js
+++ src/BrewerContact.js
@@ -8,13 +8,23 @@ class BrewerContact extends Component {
       <div className={'brewer-contact ' + (this.props.brewerInfo ? '' : 'hidden')}>
         {/* TODO - double check this html format and also style it */}
         <address>
-            {this.props.brewerInfo.address}, {this.props.brewerInfo.city}, {this.props.brewerInfo.state}
+            {this._renderAddress()}
             {/* TODO - make this into its own component? */}
             <span className={(this.props.brewerInfo.country === 'USA' ? 'hidden' : '' )}>{this.props.brewerInfo.country}</span>
         </address>
         <Telephone phone={this.props.brewerInfo.phone} />
       </div>
     );
+  }
+
+  _renderAddress() {
+    const addr = [
+        this.props.brewerInfo.address,
+        this.props.brewerInfo.city,
+        this.props.brewerInfo.state
+    ];
+    // We filter out "falsy" empty strings
+    return addr.filter(x => x).join(', ');
   }
 }
 
