4b1bb50b8e245bf98e208260942904424b27da55
Chris Russell-Walker <c.russell.walker@gmail.com>
2017-03-26 20:30:51 -0400

    Dev and prod servers are diff. using simple express for prod.

--- config/customizers.js
+++ config/customizers.js
@@ -29,38 +29,6 @@ module.exports = {
       }
     }
   },
-  'LESS': {
-    toArray: 'loaders',
-    fileRegex: /\.less$/,
-    getDev: function () {
-      return {
-        test: /\.less$/,
-        loader: "style!css!postcss!less"
-      }
-    },
-    getProd: function () {
-      return {
-        test: /\.less/,
-        loader: ExtractTextPlugin.extract('style', 'css!postcss!less')
-      }
-    }
-  },
-  'STYLUS': {
-    toArray: 'loaders',
-    fileRegex: /\.styl$/,
-    getDev: function () {
-      return {
-        test: /\.styl/,
-        loader: 'style!css!postcss!stylus'
-      }
-    },
-    getProd: function () {
-      return {
-        test: /\.styl/,
-        loader: ExtractTextPlugin.extract('style', 'css!postcss!stylus')
-      }
-    }
-  },
   'CSS_MODULES': {
     config: {
       dev: 'style!css?modules&camelCase&importLoaders=1&localIdentName=[name]__[local]___[hash:base64:5]!postcss',
---
+++ scripts/server.js
@@ -0,0 +1,20 @@
+process.env.NODE_ENV = 'production';
+
+var path = require('path');
+var express = require('express');
+var webpack = require('webpack');
+var config = require('../config/webpack.config.prod');
+
+const app = express(),
+      DIST_DIR = path.join(__dirname, '/../build'),
+      HTML_FILE = path.join(DIST_DIR, 'index.html'),
+      DEFAULT_PORT = 3000,
+      compiler = webpack(config);
+
+app.set('port', process.env.PORT || DEFAULT_PORT);
+
+app.use(express.static(DIST_DIR));
+
+app.get('*', (req, res) => res.sendFile(HTML_FILE));
+
+app.listen(app.get('port'));
