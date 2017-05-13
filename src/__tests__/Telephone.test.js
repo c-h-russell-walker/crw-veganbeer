1c34f27a44aebedd2f3bfef58db9a5ceace457b0
Chris Russell-Walker <c.russell.walker@gmail.com>
2017-05-12 22:09:02 -0400

    Update tests for Telephone component to be more meaningful

--- src/__tests__/Telephone.test.js
+++ src/__tests__/Telephone.test.js
@@ -1,9 +1,24 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import TestUtils from 'react-addons-test-utils';
 
 import Telephone from '../app/Telephone';
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<Telephone phone={'617-555-1212'} />, div);
+describe('Telephone', () => {
+  const phoneString = '617-555-1212';
+
+  const component = TestUtils.renderIntoDocument(
+    <Telephone phone={phoneString} />
+  );
+
+  it('should have a prop with the phone number', () => {
+    expect(component.props).toMatchObject({phone: phoneString});
+  });
+
+  it('should have a method to strip non-numeric', () => {
+    expect(component._stripNonNumeric()).toBe('6175551212');
+  });
+
+  it('should have a method to render tel attribute', () => {
+    expect(component._renderPhoneHref()).toBe('tel:6175551212');
+  });
 });
